79c351651d183b301804f201836fdcbdc5704286
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2024-03-07 01:29:18 -0300

    Questão 5: Inversão de String

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        // QUESTÃO 2: FIBONACCI
         Fibonacci.printFibonacci(20);
 
+        // QUESTÃO 5: INVERSÃO DE STRING
+        ReverseString.printReverse("Gustavo");
     }
 }
